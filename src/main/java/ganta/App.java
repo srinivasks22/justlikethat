0127de21145f4c0bfbb38a777339e2fe9f9b1a57
a3109213 <a3109213@8425-26s4fb3.corp.ad.fmcna.com>
2023-05-26 14:11:44 -0400

    updated

---
+++ src/main/java/ganta/App.java
@@ -0,0 +1,8 @@
+package ganta;
+
+public class App {
+    public static String capitalize( String arg ) {
+    	System.out.println("Chamkeeela angilodeeeee.................");
+        return arg.toUpperCase();
+    }
+}
